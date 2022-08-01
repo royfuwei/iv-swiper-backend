7cfb4b2b5d57334e877b02922b0303f54a4ac2e2
royfuwei <royfuwei@gmail.com>
2022-08-01 10:19:34 +0800

    feat: create mongodb util BaseMongoRepo

---
+++ src/infrastructures/mongodb/util.ts
@@ -0,0 +1,39 @@
+import { NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import _ = require('lodash');
+
+export class BaseMongoRepo<TModel, TData> {
+  private model: Model<TModel>;
+  constructor(model: Model<TModel>) {
+    this.model = model;
+  }
+
+  async findById(id: string): Promise<TData> {
+    const result = await this.model.findById(id).exec();
+    return result.toObject<TData>({ getters: true });
+  }
+
+  async add(data: TData): Promise<TData> {
+    const create = new this.model(data);
+    const post = await create.save();
+    const result: TData = post.toObject({ getters: true });
+    return result;
+  }
+
+  async updateById(id: string, data: TData): Promise<TData> {
+    const result = await this.model
+      .findByIdAndUpdate(id, {
+        $set: data,
+      })
+      .exec();
+    if (_.isNull(result)) {
+      throw new NotFoundException(id);
+    }
+    return this.findById(id);
+  }
+
+  async findByFilter(): Promise<TData[]> {
+    const results = await this.model.find().exec();
+    return results.map((item) => item.toObject({ getters: true }));
+  }
+}
